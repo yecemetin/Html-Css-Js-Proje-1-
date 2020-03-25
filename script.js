a388ff77a4e554fcc0de80519716450b52fa26d4
Yağmur Ece Metin <36815120+yecemetin@users.noreply.github.com>
2020-03-25 19:12:04 +0300

    project pertama

---
+++ script.js
@@ -0,0 +1,64 @@
+
+$(document).ready(function () {
+
+    var key = 'AIzaSyCCgoBHH34hsh_33vkXI06OsI4-v95XzmM';
+    var playlistId = 'PLNsb3_OPP1xr1JhaVJfoC1vaiS3BTM7h_';
+    var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+
+
+    var options = {
+        part: 'snippet',
+        key: key,
+        maxResults: 20,
+        playlistId: playlistId
+    }
+
+    loadVids();
+
+    function loadVids() {
+        $.getJSON(URL, options, function (data) {
+            var id = data.items[0].snippet.resourceId.videoId;
+            mainVid(id);
+            resultsLoop(data);
+        });
+    }
+
+    function mainVid(id) {
+        $('#video').html(`
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  
+			`);
+    }
+
+		
+    function resultsLoop(data) {
+
+        $.each(data.items, function (i, item) {
+
+            var thumb = item.snippet.thumbnails.medium.url;
+            var title = item.snippet.title;
+            var desc = item.snippet.description.substring(0, 100);
+            var vid = item.snippet.resourceId.videoId;
+
+
+            $('main').append(`
+				<article class="item" data-key="${vid}">
+
+					<img src="${thumb}" alt="" class="thumb">
+					<div class="details">
+						<h4>${title}</h4>
+						<p>${desc}</p>
+					</div>
+
+				</article>
+						`);
+        });
+    }
+
+		// CLICK EVENT
+    $('main').on('click', 'article', function () {
+        var id = $(this).attr('data-key');
+        mainVid(id);
+    });
+
+
+});
